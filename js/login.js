4fe0af8215d537968a91e791b9106aa5fcdf0555
ER7136 <68866082+ER7136@users.noreply.github.com>
2020-08-12 17:30:31 -0300

    Entrega 1
    -Añadidos datos de productos en products.html.
    -Añadida pantalla de login como página principal. Nombres cambiados: "login.html" ahora es "index.html", y "index.html" ahora es "main-page.html".
    -Arreglado charset en todos los .html.

--- js/login.js
+++ js/login.js
@@ -1,6 +1,20 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e) {
+    document.getElementById("submitBtn").addEventListener("click", function(e) {
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById("inputPassword");
+        let fieldFilled = true;
 
+        if (inputEmail.value === "" || inputPassword.value === "") {
+            fieldFilled = false;
+        }
+
+        if (fieldFilled) {
+            window.location = "main-page.html";
+        } else {
+            alert("Debes ingresar los datos en ambos campos.")
+        }
+    });
 });
